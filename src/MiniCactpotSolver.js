285c27f632bdcff4dbf07531c2ebcf2eef539563
Dante Tam <dante.tam1@gmail.com>
2023-01-15 11:12:41 -0600

    Render the board

--- src/MiniCactpotSolver.js
+++ src/MiniCactpotSolver.js
@@ -14,15 +14,58 @@ class MiniCactpotSolver extends React.Component {
     }
 
     this.state = {
+      rows: this.props.rows,
+      cols: this.props.cols,
       grid: Array(this.props.rows).fill(0).map(x => Array(this.props.cols).fill(null)),
       remainingNums: nums
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
   render() {
     return (<div>
+        Here: <br/>
+        <input type="text" onChange={(e) => this.handleChange(e, 1, 2)} />
 
+        Board: <br />
+        { this.getBoard() }
       </div>);
+  }
+
+  getBoard() {
+    let rows = [];
+    for (let i = 0; i < this.state.rows; i++) {
+      let cols = [];
+      for (let j = 0; j < this.state.cols; j++) {
+        let str = this.state.grid[i][j];
+        if (str === null) {
+          str = "Hidden"
+        }
+        cols.push(<td>{str}</td>);
+      }
+      rows.push(<tr>{cols}</tr>);
+    }
+
+    const tableStyle = {
+      border: '10px solid'
+    }
+
+    return (
+      <table style={tableStyle}>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
+    );
+  }
+
+  handleChange(event, row, col) {
+    if (event.target !== undefined) {
+      console.log("item: " + event.target.value);
+      console.log(row);
+
+    }
   }
 
   componentDidMount() {
